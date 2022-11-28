826f6b9e524d09c10b7c4213bd6d786b4e398ddf
= <=>
2022-11-28 19:24:41 -0400

    starting home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import styles from "../styles/pages/home.module.css";
+import Header from "../common/ui/Header";
+import Navbar from "../common/ui/Navbar";
+import Button from "../common/ui/Button";
 
 const index = () => {
   return (
-    <div>portfolio</div>
-  )
-}
+    <>
+      <Header center={true} background={true}>
+        {() => (
+          <>
+            <Navbar />
+            <h1 className={styles.header_title}>
+              <span>Hello</span> i'm Andres
+            </h1>
+            <p className={styles.header_subtitle}>
+              I'm a backend developer with specialized experience with JS and
+              Node.js enviroments
+            </p>
+            <Button>
+              Read more
+            </Button>
+          </>
+        )}
+      </Header>
+    </>
+  );
+};
 
-export default index
+export default index;
