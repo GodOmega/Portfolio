4c5d7f80c9ec64630d07647c3cc8561fe06384e7
= <=>
2022-11-28 17:34:44 -0400

    adding google fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,24 @@
-import 'normalize.css/normalize.css'
-import '../styles/global.css'
+import { Roboto } from "@next/font/google";
+import "normalize.css/normalize.css";
+import "../styles/global.css";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700", "900"],
+  style: ["normal"],
+  subsets: ["spanish"],
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${roboto.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
