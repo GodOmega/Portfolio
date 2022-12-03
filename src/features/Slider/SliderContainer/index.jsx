c03424cdb9bdcb543d32886685c2f65e869d8044
= <=>
2022-12-03 17:32:48 -0400

    Create Slider Component

---
+++ src/features/Slider/SliderContainer/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const SliderContainer = ({ children }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  };
+  return <Slider {...settings}>{children}</Slider>;
+};
+
+export default SliderContainer;
