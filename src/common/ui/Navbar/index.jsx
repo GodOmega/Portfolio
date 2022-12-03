c6bdd17fa0c33c548041392dc8676e18ca1e3a4f
= <=>
2022-12-03 17:54:47 -0400

    fixing navbar responsive

--- src/common/ui/Navbar/index.jsx
+++ src/common/ui/Navbar/index.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import {Link} from 'react-scroll'
+import React from "react";
+import { Link } from "react-scroll";
 
-import styles from './style.module.css'
+import styles from "./style.module.css";
 
 const Navbar = () => {
-  const smooth = true
-  const duration = 500
+  const smooth = true;
+  const duration = 500;
   return (
     <nav className={styles.navbar}>
-        <ul className={styles.navbar__container}>
-            <li>
-              <Link to="about" smooth={smooth} duration={duration}>about</Link>
-            </li>
-            <li>
-              <Link to="about" smooth={smooth} duration={duration}>portfolio</Link>
-            </li>
-            <li>
-              <Link to="about" smooth={smooth} duration={duration}>contact</Link>
-            </li>
-        </ul>
+      <ul className={styles.navbar__container}>
+        <li>
+          <Link to="about" smooth={smooth} duration={duration}>
+            about
+          </Link>
+        </li>
+        <li>
+          <Link to="portfolio" smooth={smooth} duration={duration}>
+            portfolio
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={smooth} duration={duration}>
+            contact
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
